ccb3b308d7acaafe9edaa20f0742ae38602ad96c
lytictail <josh@lytictail>
2017-11-10 05:45:35 +0000

    Lytictail Commit

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "lytictail")
+set(CN_CURRENCY_DISPLAY_NAME "Lytictail")
+set(CN_CURRENCY_TICKER "Lytictail")
